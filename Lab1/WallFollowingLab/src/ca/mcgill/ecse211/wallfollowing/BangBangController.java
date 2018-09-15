deab75f9430aa214ebf8969b231e528f01a50554
Reem Madkour <reem.madkour@mail.mcgill.ca>
2018-09-15 15:22:44 -0400

    test

---
+++ Lab1/WallFollowingLab/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -0,0 +1,63 @@
+package ca.mcgill.ecse211.wallfollowing;
+
+public class BangBangController implements UltrasonicController {
+
+  private final int bandCenter; 
+  private final int bandWidth;
+  private final int motorLow;
+  private final int motorHigh;
+  private int distance;
+
+  public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
+    // Default Constructor
+    this.bandCenter = bandCenter;
+    this.bandWidth = bandwidth;
+    this.motorLow = motorLow;
+    this.motorHigh = motorHigh;
+    WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
+    WallFollowingLab.rightMotor.setSpeed(motorHigh);
+    WallFollowingLab.leftMotor.forward();
+    WallFollowingLab.rightMotor.forward();
+  }
+
+  @Override
+  public void processUSData(int distance) {
+    this.distance = distance;
+    int distError=bandCenter-distance;// Compute error 
+    if (distance>2000) {
+    WallFollowingLab.leftMotor.setSpeed(40);// Start moving forward 
+	WallFollowingLab.rightMotor.setSpeed(75); 
+	WallFollowingLab.leftMotor.backward(); 
+	WallFollowingLab.rightMotor.backward();}
+    
+       
+    else if (Math.abs(distError) <= bandWidth ) {// Within limits, same speed 
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh);// Start moving forward 
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh+100); 
+    	WallFollowingLab.leftMotor.forward(); 
+    	WallFollowingLab.rightMotor.forward();}
+    else if (distError > 0) {// Too close to the wall 
+    		/**if (distance<5) {WallFollowingLab.leftMotor.setSpeed(40);// Start moving forward 
+    		WallFollowingLab.rightMotor.setSpeed(75); 
+    		WallFollowingLab.leftMotor.backward(); 
+    		WallFollowingLab.rightMotor.backward();}**/
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh+100-50); 
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh+motorLow+90); 
+    	WallFollowingLab.rightMotor.forward(); 
+    	WallFollowingLab.leftMotor.forward();}
+    else if (distError < 0) { //too far
+    	
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh+100+30); 
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh-motorLow); 
+    	WallFollowingLab.rightMotor.forward(); 
+    	WallFollowingLab.leftMotor.forward();} 
+    //Thread.sleep(50);//  Allow other threads to get CPU
+    }
+    // TODO: process a movement based on the us distance passed in (BANG-BANG style)
+  
+
+  @Override
+  public int readUSDistance() {
+    return this.distance;
+  }
+}
---
+++ Lab1/WallFollowingLab/src/ca/mcgill/ecse211/wallfollowing/PController.java
@@ -0,0 +1,88 @@
+package ca.mcgill.ecse211.wallfollowing;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class PController implements UltrasonicController {
+
+  /* Constants */
+  private static final int MOTOR_SPEED = 200;
+  private static final int FILTER_OUT = 20;
+
+
+  private final int bandCenter;
+  private final int bandWidth;
+  private int distance;
+  private int filterControl;
+
+  public PController(int bandCenter, int bandwidth) {
+    this.bandCenter = bandCenter;
+    this.bandWidth = bandwidth;
+    this.filterControl = 0;
+
+    WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED); // Initalize motor rolling forward
+    WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED+100);
+    WallFollowingLab.leftMotor.forward();
+    WallFollowingLab.rightMotor.forward();
+  }
+
+  @Override
+  public void processUSData(int distance) {
+
+    // rudimentary filter - toss out invalid samples corresponding to null
+    // signal.
+    // (n.b. this was not included in the Bang-bang controller, but easily
+    // could have).
+    //
+	  //if (distance>2000) {}
+	   if (distance >= 255 && filterControl < FILTER_OUT) {
+      // bad value, do not set the distance var, however do increment the
+      // filter value
+      filterControl++;
+    } else if (distance >= 255) {
+      // We have repeated large values, so there must actually be nothing
+      // there: leave the distance alone
+      this.distance = distance;
+    } else {
+      // distance went below 255: reset filter and leave
+      // distance alone.
+      filterControl = 0;
+      this.distance = distance;
+    }
+	   this.distance = distance;
+	    int distError=bandCenter-distance;// Compute error 
+	    if (distance>2000) {
+	    WallFollowingLab.leftMotor.setSpeed(40);// Start moving forward 
+		WallFollowingLab.rightMotor.setSpeed(75); 
+		WallFollowingLab.leftMotor.backward(); 
+		WallFollowingLab.rightMotor.backward();}
+	    
+	       
+	    else if (Math.abs(distError) <= bandWidth ) {// Within limits, same speed 
+	    	WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED);// Start moving forward 
+	    	WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED+100); 
+	    	WallFollowingLab.leftMotor.forward(); 
+	    	WallFollowingLab.rightMotor.forward();}
+	    else if (distError > 0) {// Too close to the wall 
+	
+	    	WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED+100-50); 
+	    	WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED+5*(distError)); 
+	    	WallFollowingLab.rightMotor.forward(); 
+	    	WallFollowingLab.leftMotor.forward();}
+	    else if (distError < 0) { //too far
+	    	
+	    	WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED+100+30+5*(distError)); 
+	    	WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED-7*(distError)); 
+	    	WallFollowingLab.rightMotor.forward(); 
+	    	WallFollowingLab.leftMotor.forward();} 
+	    //Thread.sleep(50);//  Allow other threads to get CPU
+	    }
+    // TODO: process a movement based on the us distance passed in (P style)
+  
+
+
+  @Override
+  public int readUSDistance() {
+    return this.distance;
+  }
+
+}
